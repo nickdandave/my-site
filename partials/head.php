15ebb939fd771a48d14d7e313b1c17e769416085
Nick Davidson <nick.davidson@whiteboard.is>
2017-06-28 15:51:39 -0400

    experiment with onload attr

--- partials/head.php
+++ partials/head.php
@@ -26,7 +26,7 @@
         <style>
             <?php include("assets/dist/css/critical.css"); ?>
         </style>
-        <link rel="preload" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.css" as="style" onload="this.rel='stylesheet'">
+        <link rel="preload" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.css" as="style" onload="this.rel='stylesheet';console.log('flex loaded')">
         <link rel="preload" href="/assets/dist/css/app.min.css" as="style" onload="this.rel='stylesheet'">
         <noscript>
             <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.css">
